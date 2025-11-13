b04448bd00f9b39c430475ea68b88ad09dcb5050
Griffin Michalak <81431781+GriffinMichalak@users.noreply.github.com>
2025-11-12 20:09:36 -0500

    remove some comments

--- helpers.py
+++ helpers.py
@@ -6,30 +6,11 @@ nltk.download('punkt')
 SENTENCE_BEGIN = "<s>"
 SENTENCE_END = "</s>"
 
-
 def tokenize_line(line: str, ngram: int, 
                    by_char: bool = True, 
                    space_char: str = ' ',
                    sentence_begin: str=SENTENCE_BEGIN, 
                    sentence_end: str=SENTENCE_END):
-  """
-  Tokenize a single string. Glue on the appropriate number of 
-  sentence begin tokens and sentence end tokens (ngram - 1), except
-  for the case when ngram == 1, when there will be one sentence begin
-  and one sentence end token.
-  Args:
-    line (str): text to tokenize
-    ngram (int): ngram preparation number
-    by_char (bool): default value True, if True, tokenize by character, if
-      False, tokenize by whitespace
-    space_char (str): if by_char is True, use this character to separate to replace spaces
-    sentence_begin (str): sentence begin token value
-    sentence_end (str): sentence end token value
-
-  Returns:
-    list of strings - a single line tokenized
-  """
-  # PROVIDED
   inner_pieces = None
   if by_char:
     line = line.replace(' ', space_char)
@@ -52,8 +33,6 @@ def read_file(datapath, ngram, by_character=False):
     with open(datapath) as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            # THIS IS WHERE WE GET CHARACTERS INSTEAD OF WORDS
-            # replace spaces with underscores
             data.append([
               tokenize_line(row['clean_text'].lower(), ngram, by_char=by_character, space_char="_"),
               int(row['is_depression'])
