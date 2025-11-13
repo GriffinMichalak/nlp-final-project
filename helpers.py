857b4dfa6016e555d53a82837b27a90dd46696a8
Griffin Michalak <81431781+GriffinMichalak@users.noreply.github.com>
2025-11-12 21:02:52 -0500

    get_stats

--- helpers.py
+++ helpers.py
@@ -1,5 +1,6 @@
 import nltk
 import csv
+from sklearn.model_selection import train_test_split
 
 nltk.download('punkt')
 
@@ -38,3 +39,44 @@ def read_file(datapath, ngram, by_character=False):
               int(row['is_depression'])
             ])
     return data
+
+def split(data, dist="80/10/10"):
+  print(f"Completing {dist} split")
+  n = len(data)
+  test_size = 0.80
+  if dist == "70/15/15":
+    test_size = 0.70
+
+  train, b = train_test_split(data, test_size=test_size, random_state=42)
+  dev, test = train_test_split(train, test_size=0.50, random_state=42)
+
+  return train, dev, test
+
+
+def get_stats(data):
+  # get analytics about data
+  n = len(data)
+
+  tokens = []
+  vocab = set()
+  for row in data:
+      for word in row[0]:
+          vocab.add(word)
+          tokens.append(word)
+
+  # calculate pos and neg depression classifications
+  dep_pos = 0
+  dep_neg = 0
+  for row in data:
+      if row[1] == 1:
+          dep_pos += 1
+      if row[1] == 0:
+          dep_neg += 1
+
+  print("======================================================================")
+  print(f"Number of lines: {n}")
+  print(f"Number of tokens: {len(tokens)}")
+  print(f"Number of unique tokens: {len(vocab)}")
+  print(f"Number of YES depression entries: {dep_pos} ({dep_pos/n * 100:2.2f}%)")
+  print(f"Number of NOT depression entries: {dep_neg} ({dep_neg/n * 100:2.2f}%)")
+  print("======================================================================")
