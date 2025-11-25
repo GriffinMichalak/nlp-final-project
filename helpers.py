dcd34bfc3d60518ad1f72d9a6fafec8a27a40c56
Sean O'Donnell <odonnell.sea@northeastern.edu>
2025-11-25 13:28:34 -0500

    Added Naive Bayes metrics to results.csv and testing notebook

--- helpers.py
+++ helpers.py
@@ -16,43 +16,44 @@ SENTENCE_END = "</s>"
 
 # words that aren't real or don't add value
 more_stop_words = [
-  'depression',
-  'pression',
-  'co',
-  'http',
-  'mizzzidc',
-  'even',
-  'quot',
-  'com',
-  'tinyurl',
-  'twitpic',
-  'yeah',
+    'depression',
+    'pression',
+    'co',
+    'http',
+    'mizzzidc',
+    'even',
+    'quot',
+    'com',
+    'tinyurl',
+    'twitpic',
+    'yeah',
 ]
 
 stop_words = set(stopwords.words('english'))
 
 for w in more_stop_words:
-  stop_words.add(w)
+    stop_words.add(w)
 
-def tokenize_line(line: str, ngram: int, 
-                   by_char: bool = True, 
-                   space_char: str = ' ',
-                   sentence_begin: str=SENTENCE_BEGIN, 
-                   sentence_end: str=SENTENCE_END):
-  inner_pieces = None
-  if by_char:
-    line = line.replace(' ', space_char)
-    inner_pieces = list(line)
-  else:
-    # otherwise use nltk's word tokenizer
-    inner_pieces = nltk.word_tokenize(line)
 
-  if ngram == 1:
-    tokens = [sentence_begin] + inner_pieces + [sentence_end]
-  else:
-    tokens = ([sentence_begin] * (ngram - 1)) + inner_pieces + ([sentence_end] * (ngram - 1))
-  # always count the unigrams
-  return tokens
+def tokenize_line(line: str, ngram: int,
+                  by_char: bool = True,
+                  space_char: str = ' ',
+                  sentence_begin: str = SENTENCE_BEGIN,
+                  sentence_end: str = SENTENCE_END):
+    inner_pieces = None
+    if by_char:
+        line = line.replace(' ', space_char)
+        inner_pieces = list(line)
+    else:
+        # otherwise use nltk's word tokenizer
+        inner_pieces = nltk.word_tokenize(line)
+
+    if ngram == 1:
+        tokens = [sentence_begin] + inner_pieces + [sentence_end]
+    else:
+        tokens = ([sentence_begin] * (ngram - 1)) + inner_pieces + ([sentence_end] * (ngram - 1))
+    # always count the unigrams
+    return tokens
 
 
 def get_data(datapath, ngram=2, tokenize=True, by_character=False):
@@ -68,7 +69,8 @@ def get_data(datapath, ngram=2, tokenize=True, by_character=False):
             if tokenize:
                 tokens = tokenize_line(text, ngram, by_char=by_character, space_char="_")
                 if not by_character:
-                    tokens = [token for token in tokens if token not in stop_words or token in [SENTENCE_BEGIN, SENTENCE_END]]
+                    tokens = [token for token in tokens if
+                              token not in stop_words or token in [SENTENCE_BEGIN, SENTENCE_END]]
                 clean_text.append(tokens)
             else:
                 if not by_character:
@@ -77,76 +79,79 @@ def get_data(datapath, ngram=2, tokenize=True, by_character=False):
             is_depression.append(int(row['is_depression']))
         return clean_text, is_depression
 
+
 def split(data, dist="80/10/10"):
-  print(f"Completing {dist} split")
-  test_size = 0.80
-  if dist == "70/15/15":
-    test_size = 0.70
+    print(f"Completing {dist} split")
+    test_size = 0.80
+    if dist == "70/15/15":
+        test_size = 0.70
 
-  train, temp = train_test_split(data, test_size=(1 - test_size), random_state=42)
-  dev, test = train_test_split(temp, test_size=0.50, random_state=42)
+    train, temp = train_test_split(data, test_size=(1 - test_size), random_state=42)
+    dev, test = train_test_split(temp, test_size=0.50, random_state=42)
 
-  return train, dev, test
+    return train, dev, test
 
 
 def get_stats(data):
-  # get analytics about data
-  n = len(data)
+    # get analytics about data
+    n = len(data)
 
-  tokens = []
-  vocab = set()
-  for row in data:
-      for word in row[0]:
-          vocab.add(word)
-          tokens.append(word)
+    tokens = []
+    vocab = set()
+    for row in data:
+        for word in row[0]:
+            vocab.add(word)
+            tokens.append(word)
 
-  # calculate pos and neg depression classifications
-  dep_pos = 0
-  dep_neg = 0
-  for row in data:
-      if row[1] == 1:
-          dep_pos += 1
-      if row[1] == 0:
-          dep_neg += 1
+    # calculate pos and neg depression classifications
+    dep_pos = 0
+    dep_neg = 0
+    for row in data:
+        if row[1] == 1:
+            dep_pos += 1
+        if row[1] == 0:
+            dep_neg += 1
 
-  print("======================================================================")
-  print(f"Number of lines: {n}")
-  print(f"Number of tokens: {len(tokens)}")
-  print(f"Number of unique tokens: {len(vocab)}")
-  print(f"Number of YES depression entries: {dep_pos} ({dep_pos/n * 100:2.2f}%)")
-  print(f"Number of NOT depression entries: {dep_neg} ({dep_neg/n * 100:2.2f}%)")
-  print("======================================================================")
+    print("======================================================================")
+    print(f"Number of lines: {n}")
+    print(f"Number of tokens: {len(tokens)}")
+    print(f"Number of unique tokens: {len(vocab)}")
+    print(f"Number of YES depression entries: {dep_pos} ({dep_pos / n * 100:2.2f}%)")
+    print(f"Number of NOT depression entries: {dep_neg} ({dep_neg / n * 100:2.2f}%)")
+    print("======================================================================")
+
 
 def save_to_csv(file_name: str, row: list):
-  if os.path.exists(file_name):
-      with open(file_name, 'a', newline='') as csvfile:
-          writer = csv.writer(csvfile)
-          writer.writerow(row)
-      print(f"Data appended to {file_name}")
-  else:
-      header = ["model", "precision", "recall", "f1", "accuracy", "pr_auc", "roc_auc"]
-      data = [row]
+    if os.path.exists(file_name):
+        with open(file_name, 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(row)
+        print(f"Data appended to {file_name}")
+    else:
+        header = ["model", "precision", "recall", "f1", "accuracy", "pr_auc", "roc_auc"]
+        data = [row]
 
-      with open(file_name, 'w', newline='') as csvfile:
-          writer = csv.writer(csvfile)
-          writer.writerow(header)
-          writer.writerow(row)
+        with open(file_name, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(header)
+            writer.writerow(row)
 
-      print(f"Created file {file_name}")
+        print(f"Created file {file_name}")
+
 
 def show_image_group(images: list, title: str, rows: int = 2, cols: int = 2):
-  fig, axes = plt.subplots(rows, cols, figsize=(8, 8)) 
+    fig, axes = plt.subplots(rows, cols, figsize=(8, 8))
 
-  axes = axes.flatten()
+    axes = axes.flatten()
 
-  for i, img_path in enumerate(images):
-      if i < len(axes) and os.path.exists(img_path):
-          img = mpimg.imread(img_path)
-          axes[i].imshow(img)
-          axes[i].axis('off')
-      else:
-          axes[i].axis('off')
+    for i, img_path in enumerate(images):
+        if i < len(axes) and os.path.exists(img_path):
+            img = mpimg.imread(img_path)
+            axes[i].imshow(img)
+            axes[i].axis('off')
+        else:
+            axes[i].axis('off')
 
-  fig.suptitle(title, fontsize=16)
+    fig.suptitle(title, fontsize=16)
 
-  plt.show()
+    plt.show()
